4d7c014dabb531a8e1c06b2a5e4b16f1dca8dd83
Oleh Martsokha <o.martsokha@gmail.com>
2024-06-24 23:27:05 +0200

    ⚙️ runtime.Version

--- vars.go
+++ vars.go
@@ -3,13 +3,21 @@ package glide
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"strings"
 )
 
 // ClientVersion is the current version of this client.
 var clientVersion = "0.1.0"
 
 // GoVersion is the required version of the Go runtime.
-var goVersion = "1.22.2"
+var goVersion = getVersion()
+
+func getVersion() string {
+	version := runtime.Version()
+	after, _ := strings.CutPrefix(version, "go")
+	return after
+}
 
 // userAgent is a default User-Agent header value.
 var userAgent = fmt.Sprintf("Glide/%s (Go; Ver. %s)", clientVersion, goVersion)
